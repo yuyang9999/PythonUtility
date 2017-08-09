ca4b7a708beba6b465bbd27a809c67f09e2920e4
yuyang <yuyang@yuyangs-MacBook-Pro.local>
2017-08-09 10:36:29 +0800

    add sql utility

---
+++ Utility/SqlUtility.py
@@ -0,0 +1,15 @@
+importMySQLdb
+
+class MySQLConn:
+	def__init__(self,host,db_name,user_name,password):
+	conn=MySQLdb.connect(host=host,user=user_name,passwd=password,db=db_name)
+	conn.set_character_set('utf8')
+
+	cursor=conn.cursor()
+
+	cursor.execute('SETNAMESutf8;')
+	cursor.execute('SETCHARACTERSETutf8;')
+	cursor.execute('SETcharacter_set_connection=utf8;')
+
+	self.conn=conn
+	self.cursor=cursor
--- Utility/__init__.py
+++ Utility/__init__.py
@@ -1,3 +1,2 @@
 from .MpUtility import mutli_processing
-
-
+from .SqlUtility import MySQLConn
