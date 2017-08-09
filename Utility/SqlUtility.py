7a265d04f63d15d1606bb5f46e509f471a9dd577
yuyang <yuyang@yuyangs-MacBook-Pro.local>
2017-08-09 14:04:28 +0800

    fix code bug

--- Utility/SqlUtility.py
+++ Utility/SqlUtility.py
@@ -1,15 +1,15 @@
-importMySQLdb
+import MySQLdb
 
 class MySQLConn:
-	def__init__(self,host,db_name,user_name,password):
-	conn=MySQLdb.connect(host=host,user=user_name,passwd=password,db=db_name)
-	conn.set_character_set('utf8')
+	def __init__(self,host,db_name,user_name,password):
+		conn=MySQLdb.connect(host=host,user=user_name,passwd=password,db=db_name)
+		conn.set_character_set('utf8')
 
-	cursor=conn.cursor()
+		cursor=conn.cursor()
 
-	cursor.execute('SETNAMESutf8;')
-	cursor.execute('SETCHARACTERSETutf8;')
-	cursor.execute('SETcharacter_set_connection=utf8;')
+		cursor.execute('SETNAMESutf8;')
+		cursor.execute('SETCHARACTERSETutf8;')
+		cursor.execute('SETcharacter_set_connection=utf8;')
 
-	self.conn=conn
-	self.cursor=cursor
+		self.conn=conn
+		self.cursor=cursor
