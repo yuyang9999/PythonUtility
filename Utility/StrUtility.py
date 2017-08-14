f8bd48f8c81ebc14fa7aabb42483aff2419641f3
yuyang <yuyang@yuyangs-MacBook-Pro.local>
2017-08-14 11:38:14 +0800

    adjust str utility

--- Utility/StrUtility.py
+++ Utility/StrUtility.py
@@ -1,8 +1,5 @@
 import re
 
-
-
-
 class StrUtility:
 	bracket_pattern = re.compile(r'[\[【\(（].*[\]】\)）]')
 
@@ -13,38 +10,10 @@ class StrUtility:
                            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
                            "]+", flags=re.UNICODE)
 
-	start_puncation_pattern = re.compile(r'^[.]')
-	
+	start_puncation_pattern = re.compile(r'^[\.,?"`~\'。，？]+')
+	end_puncation_pattern = re.compile(r'[\.?"`~\'。，？]+$')
+
 	@staticmethod
 	def remove_str_bracket(str):
 		return bracket_pattern.sub('', str)
 	
-
-	@staticmethod
-	def strQ2B(ustring):
-	    rstring = ""
-	    for uchar in ustring:
-	        inside_code=ord(uchar)
-	        if inside_code==0x3000:
-	            inside_code=0x0020
-	        else:
-	            inside_code-=0xfee0
-	        if inside_code<0x0020 or inside_code>0x7e:      #转完之后不是半角字符返回原来的字符
-	            rstring += uchar
-	        rstring += unichr(inside_code)
-	    return rstring
-
-
-	@staticmethod
-	def strB2Q(ustring):
-	    rstring = ""
-	    for uchar in ustring:
-	        inside_code=ord(uchar)
-	        if inside_code<0x0020 or inside_code>0x7e:      #不是半角字符就返回原来的字符
-	            rstring += uchar
-	        if inside_code==0x0020: #除了空格其他的全角半角的公式为:半角=全角-0xfee0
-	            inside_code=0x3000
-	        else:
-	            inside_code+=0xfee0
-	        rstring += unichr(inside_code)
-	    return rstring
